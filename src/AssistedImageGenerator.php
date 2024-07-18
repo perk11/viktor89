f19bc83a9da4db1b0402e18b7db65d04a28bd8ed
Konstantin Pereiaslov <perk11@perk11.info>
2024-07-17 22:17:43 -0500

    Improvements to image assist prompt

--- src/AssistedImageGenerator.php
+++ src/AssistedImageGenerator.php
@@ -37,7 +37,7 @@ class AssistedImageGenerator implements Prompt2ImgGenerator, PromptAndImg2ImgGen
 
     private function processPrompt(string $originalPrompt): string
     {
-        $systemPrompt = "You are Gemma. Given a message from the user, reword and expand on it in a way that describes an image. This text will be used to generate an image using automatic text to image generator. Your output should contain only a literal description of the image in a single sentence. Be brief since long outputs are not handled very well. Only describe what an observer will see. Your output will be directly passed to an API, so don't output anything extra. Do not use any syntax or code formatting, just output raw text describing the image and nothing else. Translate the output to English.";
+        $systemPrompt = "You are Gemma. Given a message from the user, add details, reword and expand on it in a way that describes an image illustrating user's message.  This text will be used to generate an image using automatic text to image generator that does not understand emotions, metaphors or negatives. Your output should contain only a literal description of the image in a single sentence. Only describe what an observer will see. Your output will be directly passed to an API, so don't output anything extra. Do not use any syntax or code formatting, just output raw text describing the image and nothing else. Translate the output to English.";
         $prompt = "$systemPrompt\n\nUser: $originalPrompt\nGemma: ";
         return $this->getCompletion($prompt);
     }
