0ec414d2ecf1f23288e690c23a1b5c796d94937e
Konstantin Pereiaslov <perk11@perk11.info>
2024-07-23 23:31:25 -0500

    Implement inference server for HunyuanDiT

---
+++ inference-servers/HunyuanDiT/main.py
@@ -0,0 +1,63 @@
+import json
+import threading
+
+from flask import Flask, request, jsonify
+from diffusers import HunyuanDiTPipeline
+import torch
+import base64
+from io import BytesIO
+
+app = Flask(__name__)
+
+# Load the model
+pipeline = HunyuanDiTPipeline.from_pretrained("Tencent-Hunyuan/HunyuanDiT-v1.2-Diffusers", torch_dtype=torch.float16)
+pipeline.to("cuda")
+generator = torch.Generator()
+sem = threading.Semaphore()
+@app.route('/sdapi/v1/txt2img', methods=['POST'])
+def generate_image():
+    data = request.json
+    print(data)
+
+    prompt = data.get('prompt')
+    seed = int(data.get('seed', 0))
+    steps = int(data.get('steps', 50))
+    width = int(data.get('width', 1024))
+    height = int(data.get('height', 1024))
+
+    # Generate image
+    if seed == 0:
+        seed = generator.seed()
+    else:
+        generator.manual_seed(seed)
+    with torch.no_grad():
+        sem.acquire()
+        image = pipeline(
+            prompt=prompt,
+            height=height,
+            width=width,
+            num_inference_steps=steps,
+            generator=generator,
+        ).images[0]
+        sem.release()
+
+    # Convert image to base64
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    response = {
+        'images': [image_base64],
+        'parameters': {},
+        'info': json.dumps({
+            'infotexts': [f'{prompt}\nSteps: {steps}, Seed: {seed}, Size: {width}x{height}, Model: HunyuanDiT v1.2']
+        })
+    }
+    # Clear cache
+    torch.cuda.empty_cache()
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=18091)
