5a6bbce2e28bd4cf0f40fcd4023d65aca1d57aad
Konstantin Pereiaslov <perk11@perk11.info>
2024-10-25 02:56:26 -0500

    Move flux workflow to a separate file

--- inference-servers/flux/main.py
+++ inference-servers/flux/main.py
@@ -8,6 +8,7 @@ import threading
 import urllib.parse
 import urllib.request
 import uuid
+from pathlib import Path
 
 import websocket  # NOTE: websocket-client (https://github.com/websocket-client/websocket-client)
 from flask import Flask, request, jsonify
@@ -76,165 +77,9 @@ def generate_image():
     width = int(data.get('width', 1024))
     height = int(data.get('height', 1024))
 
-    comfy_workflow = """{
-      "5": {
-        "inputs": {
-          "width": 1024,
-          "height": 1024,
-          "batch_size": 1
-        },
-        "class_type": "EmptyLatentImage",
-        "_meta": {
-          "title": "Empty Latent Image"
-        }
-      },
-      "6": {
-        "inputs": {
-          "text": "prompt goes here",
-          "clip": [
-            "11",
-            0
-          ]
-        },
-        "class_type": "CLIPTextEncode",
-        "_meta": {
-          "title": "CLIP Text Encode (Prompt)"
-        }
-      },
-      "8": {
-        "inputs": {
-          "samples": [
-            "13",
-            0
-          ],
-          "vae": [
-            "10",
-            0
-          ]
-        },
-        "class_type": "VAEDecode",
-        "_meta": {
-          "title": "VAE Decode"
-        }
-      },
-      "10": {
-        "inputs": {
-          "vae_name": "ae.sft"
-        },
-        "class_type": "VAELoader",
-        "_meta": {
-          "title": "Load VAE"
-        }
-      },
-      "11": {
-        "inputs": {
-          "clip_name1": "t5xxl_fp16.safetensors",
-          "clip_name2": "clip_l.safetensors",
-          "type": "flux"
-        },
-        "class_type": "DualCLIPLoader",
-        "_meta": {
-          "title": "DualCLIPLoader"
-        }
-      },
-      "12": {
-        "inputs": {
-          "unet_name": "flux1-dev.sft",
-          "weight_dtype": "default"
-        },
-        "class_type": "UNETLoader",
-        "_meta": {
-          "title": "Load Diffusion Model"
-        }
-      },
-      "13": {
-        "inputs": {
-          "noise": [
-            "25",
-            0
-          ],
-          "guider": [
-            "22",
-            0
-          ],
-          "sampler": [
-            "16",
-            0
-          ],
-          "sigmas": [
-            "17",
-            0
-          ],
-          "latent_image": [
-            "5",
-            0
-          ]
-        },
-        "class_type": "SamplerCustomAdvanced",
-        "_meta": {
-          "title": "SamplerCustomAdvanced"
-        }
-      },
-      "16": {
-        "inputs": {
-          "sampler_name": "euler"
-        },
-        "class_type": "KSamplerSelect",
-        "_meta": {
-          "title": "KSamplerSelect"
-        }
-      },
-      "17": {
-        "inputs": {
-          "scheduler": "simple",
-          "steps": 20,
-          "denoise": 1,
-          "model": [
-            "12",
-            0
-          ]
-        },
-        "class_type": "BasicScheduler",
-        "_meta": {
-          "title": "BasicScheduler"
-        }
-      },
-      "22": {
-        "inputs": {
-          "model": [
-            "12",
-            0
-          ],
-          "conditioning": [
-            "6",
-            0
-          ]
-        },
-        "class_type": "BasicGuider",
-        "_meta": {
-          "title": "BasicGuider"
-        }
-      },
-      "25": {
-        "inputs": {
-          "noise_seed": 59383888452593
-        },
-        "class_type": "RandomNoise",
-        "_meta": {
-          "title": "RandomNoise"
-        }
-      },
-      "save_image_websocket_node": {
-        "class_type": "SaveImageWebsocket",
-        "inputs": {
-            "images": [
-                "8",
-                0
-            ]
-        }
-      }
-    }
-    """
+    workflow_file_path = Path(__file__).with_name("comfy_workflow.json")
+    with workflow_file_path.open('r') as workflow_file:
+        comfy_workflow = workflow_file.read()
 
     comfy_workflow_object = json.loads(comfy_workflow)
     comfy_workflow_object["5"]["inputs"]["width"] = width
