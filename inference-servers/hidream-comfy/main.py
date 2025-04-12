5b8bb0cf36d02e0ec04aafe38c73ab9f2aea2dd5
Konstantin Pereiaslov <perk11@perk11.info>
2025-04-12 15:00:29 -0500

    img2img for hidream inference server

--- inference-servers/hidream-comfy/main.py
+++ inference-servers/hidream-comfy/main.py
@@ -82,7 +82,19 @@ def get_txt2img_workflow(prompt,  negative_prompt, model, width, height, seed):
     comfy_workflow_object["19"]["inputs"]['seed'] = seed
     comfy_workflow_object["19"]["inputs"]['model_type'] = model
     return comfy_workflow_object
+def get_img2img_workflow(input_image_filename, prompt,  negative_prompt, model, seed, denoising_strength):
+    workflow_file_path = Path(__file__).with_name("comfy_workflow_img2img.json")
+    with workflow_file_path.open('r') as workflow_file:
+        comfy_workflow = workflow_file.read()
+    comfy_workflow_object = json.loads(comfy_workflow)
+    comfy_workflow_object["23"]["inputs"]['primary_prompt'] = prompt
+    comfy_workflow_object["23"]["inputs"]['negative_prompt'] = negative_prompt
+    comfy_workflow_object["23"]["inputs"]['seed'] = seed
+    comfy_workflow_object["23"]["inputs"]['model_type'] = model
+    comfy_workflow_object["23"]["inputs"]['denoising_strength'] = denoising_strength
 
+    comfy_workflow_object["27"]["inputs"]['image'] = input_image_filename
+    return comfy_workflow_object
 
 @app.route('/sdapi/v1/txt2img', methods=['POST'])
 def generate_image():
@@ -114,6 +126,52 @@ def generate_image():
                     'infotexts': [f'{prompt}\n, Seed: {seed}, Size: {width}x{height}, Model: HiDream-'
                                   + model
                                   ]
+                })
+            }
+
+            return jsonify(response)
+
+    return jsonify({
+        'error': 'No images received from ComfyUI'
+    })
+@app.route('/sdapi/v1/img2img', methods=['POST'])
+def generate_img2img():
+    data = request.json
+
+    prompt = data.get('prompt')
+    negative_prompt = data.get('negative_prompt', "")
+    denoising_strength = data.get('denoising_strength', 0.8)
+    model = data.get('model', 'dev-nf4')
+    seed = int(data.get('seed', random.randint(1, 99999999999999)))
+    init_images = data.get('init_images', [])
+
+    data["init_images"]= "[omitted " + str(len(init_images)) + "]"
+    print(data)
+    images = []
+    if len(init_images) != 1:
+        return jsonify({'error': "A single init image is required"}), 400
+
+    image_data = base64.b64decode(init_images[0])
+
+    input_image_file_name = 'viktor89-hidream.jpg'
+    with open(args.comfy_ui_input_dir + '/' + input_image_file_name, 'wb') as file:
+        file.write(image_data)
+
+    comfy_workflow_object = get_img2img_workflow(input_image_file_name, prompt, negative_prompt, model, seed, denoising_strength)
+
+    try:
+        images = get_images(comfy_workflow_object)
+        print(f"{len(images)} images received from Comfy", flush=True)
+    finally:
+        sem.release()
+    for node_id in images:
+        for image_data in images[node_id]:
+            image_base64 = base64.b64encode(io.BytesIO(image_data).getbuffer()).decode('utf-8')
+            response = {
+                'images': [image_base64],
+                'parameters': {},
+                'info': json.dumps({
+                    'infotexts': [f'{prompt}\n Seed: {seed}, Model: HiDream-{model}, Denoising strength: {denoising_strength}']
                 })
             }
 
