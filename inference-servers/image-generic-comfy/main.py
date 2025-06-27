5dc247896191414ca2cb81436cc0807cac43b4e7
Konstantin Pereiaslov <perk11@perk11.info>
2025-06-27 03:53:15 -0500

    flux Kontext inference server

--- inference-servers/image-generic-comfy/main.py
+++ inference-servers/image-generic-comfy/main.py
@@ -1,7 +1,9 @@
 import argparse
+import base64
 import json
 import random
 import sys
+import threading
 from pathlib import Path
 
 from flask import Flask, request, jsonify
@@ -55,6 +57,60 @@ def generate_image():
             return jsonify({"error": "Unknown model: " + model}), 400
 
     return comfy_workflow_to_json_image_response(comfy_workflow_object, args.comfy_ui_server_address, infotext)
+
+def get_img2img_workflow_infotext_and_filename_fluxkontext(prompt, seed, steps, width, height):
+    if steps == 0:
+        steps = 20
+    input_image_filename = 'viktor89-fluxkontext.jpg'
+    workflow_file_path = Path(__file__).with_name("flux-kontext-img2img.json")
+    with workflow_file_path.open('r') as workflow_file:
+        comfy_workflow = workflow_file.read()
+    comfy_workflow_object = json.loads(comfy_workflow)
+    comfy_workflow_object["6"]["inputs"]['text'] = prompt
+    comfy_workflow_object["27"]["inputs"]['width'] = width
+    comfy_workflow_object["27"]["inputs"]['width'] = height
+    comfy_workflow_object["30"]["inputs"]['width'] = width
+    comfy_workflow_object["30"]["inputs"]['height'] = height
+    comfy_workflow_object["25"]["inputs"]['noise_seed'] = seed
+    comfy_workflow_object["25"]["inputs"]['steps'] = steps
+
+    comfy_workflow_object["41"]["inputs"]['image'] = input_image_filename
+    return comfy_workflow_object,  f'{prompt}\nSteps: {steps}, Seed: {seed}, Model: FLUX.1-Kontext-dev', input_image_filename
+
+
+sem = threading.Semaphore() #Todo use a dict with semaphores based on input image file name
+@app.route('/sdapi/v1/img2img', methods=['POST'])
+def generate_img2img():
+    data = request.json
+
+    model = data.get('model', '(blank)')
+    prompt = data.get('prompt')
+    steps = data.get('steps', 0)
+    width = int(data.get('width', 1024))
+    height = int(data.get('height', 1024))
+    seed = int(data.get('seed', random.randint(1, 99999999999999)))
+    init_images = data.get('init_images', [])
+
+    data["init_images"]= "[omitted " + str(len(init_images)) + "]"
+    print(data)
+    images = []
+    if len(init_images) != 1:
+        return jsonify({'error': "A single init image is required"}), 400
+
+    image_data = base64.b64decode(init_images[0])
+
+    match model:
+        case 'FLUX.1-Kontext-dev':
+            comfy_workflow_object, infotext, input_image_file_name = get_img2img_workflow_infotext_and_filename_fluxkontext(prompt, seed, steps, width, height)
+        case _:
+            return jsonify({"error": "Unknown model: " + model}), 400
+    sem.acquire()
+    try:
+        with open(args.comfy_ui_input_dir + '/' + input_image_file_name, 'wb') as file:
+            file.write(image_data)
+        return comfy_workflow_to_json_image_response(comfy_workflow_object, args.comfy_ui_server_address, infotext)
+    finally:
+        sem.release()
 
 if __name__ == '__main__':
     app.run(host='localhost', port=args.port)
