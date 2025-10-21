62913bebc00c3aed8105f2269d898e2898b3ca65
Konstantin Pereiaslov <perk11@perk11.info>
2025-10-21 00:46:02 -0500

    Remove parameters from VideoApiResponse

--- src/VideoGeneration/VideoApiResponse.php
+++ src/VideoGeneration/VideoApiResponse.php
@@ -4,7 +4,7 @@ namespace Perk11\Viktor89\VideoGeneration;
 
 class VideoApiResponse
 {
-    public function __construct(public array $videos, public array $parameters, public array $info)
+    public function __construct(public array $videos, public array $info)
     {
     }
 
@@ -12,7 +12,6 @@ class VideoApiResponse
     {
         $decoded = json_decode($data, true, 512, JSON_THROW_ON_ERROR);
         if (!array_key_exists('videos', $decoded) || !is_array($decoded['videos'])
-            || !array_key_exists('parameters', $decoded) || !is_array($decoded['parameters'])
             || !array_key_exists('info', $decoded) || !is_string($decoded['info'])
         ) {
             throw new \RuntimeException("Unexpected response from Video API:\n" . $$data);
@@ -20,7 +19,6 @@ class VideoApiResponse
 
         return new self(
             $decoded['videos'],
-            $decoded['parameters'],
             json_decode($decoded['info'], true, 512, JSON_THROW_ON_ERROR),
         );
     }
